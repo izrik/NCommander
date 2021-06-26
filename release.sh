572d9ffeb45a28fd09ecff64cab0476cb0b243f7
izrik <izrik@izrik.com>
2021-06-25 23:28:29 -0500

    Add a script to build the nuget package.

---
+++ release.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+VERSION=$1
+if [[ -z "$VERSION" ]]; then
+  VERSION=$(git describe --tags | sed 's/^v//' )
+  # c.f. grep AssemblyVersion Properties/AssemblyInfo.cs
+fi
+if [[ -z "$VERSION" ]]; then
+  echo "Error: Could not determine the version string." >&2
+  exit 1
+fi
+
+nuget pack NCommander.nuspec -Version $VERSION
